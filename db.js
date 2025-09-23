9eafa63dd9d98f63e6c810621a9ebccf09b08700
Pankaj282002 <100746779+Pankaj282002@users.noreply.github.com>
2025-09-23 15:07:53 +0530

    db code update

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: isProduction ? process.env.DATABASE_URL : process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 pool.connect((err, client, release) => {
